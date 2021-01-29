c038089cf3c7042dc61aeb19427497eafd16de4f
hucilluc <katehenderson@gmx.co.uk>
2021-01-29 13:18:59 +0000

    Low poly caverns imported and Glacial scene opened

---
+++ Dimly-lit cave/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/Kate/Desktop/Github/Dimly-lit-cave/Dimly-lit cave/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1660924781710541262.json
